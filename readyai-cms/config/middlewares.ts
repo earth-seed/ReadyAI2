13a38f52b3c1f937c678bce9c2ad27be63a6dc12
Bryce <brycelmerrill@gmail.com>
2025-10-22 13:25:50 +0300

    strapi to render

---
+++ readyai-cms/config/middlewares.ts
@@ -0,0 +1,18 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://res.cloudinary.com'],
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
