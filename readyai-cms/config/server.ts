72a3d7c816752dece1bd581ef3c3ef7ddd9b3fbd
Bryce <brycelmerrill@gmail.com>
2025-11-03 06:11:08 +0300

    server settings

--- readyai-cms/config/server.ts
+++ readyai-cms/config/server.ts
@@ -4,4 +4,20 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  proxy: {
+    enabled: true,
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+  },
+  cron: {
+    enabled: false,
+  },
+  admin: {
+    url: env('PUBLIC_ADMIN_URL', '/admin'),
+    serveAdminPanel: env.bool('SERVE_ADMIN', true),
+  },
+  cookie: {
+    secure: env.bool('COOKIE_SECURE', false), // Set to true only if using HTTPS
+    sameSite: 'lax',
+  },
 });
